7a41efde24ab8e1059ca80f4917d9357a8cb0e4a
nodesire7 <63827263+nodesire7@users.noreply.github.com>
2025-03-03 18:16:26 +0800

    Update __init__.py

--- plugins.v2/hh_signin/__init__.py
+++ plugins.v2/hh_signin/__init__.py
@@ -1,11 +1,4 @@
 from .main import run
 
-PLUGIN_NAME = "HHSign"
-PLUGIN_VERSION = "1.0.0"
-PLUGIN_AUTHOR = "AFEI"
-PLUGIN_DESCRIPTION = "HH论坛自动签到插件（Cookie版）"
-plugin_icon = "signin.png"
-author_url = "https://github.com/nodesire7/"
-
 def load(user_provided_cookies=None):
     return lambda: run(user_provided_cookies)
